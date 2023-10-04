c5095dfa3612b5d60606946e68e8d29f560b37ab
Wendy03 <wendysu03@gmail.com>
2023-10-04 14:25:23 +0800

    update: content

---
+++ js/truncate.js
@@ -0,0 +1,11 @@
+const h3Elements = document.querySelectorAll('h3.truncateText');
+
+h3Elements.forEach(function (h3Element) {
+  let text = h3Element.textContent.trim();
+  const characterLimit = 50;
+  if (text.length > characterLimit) {
+    const lastSpaceIndex = text.lastIndexOf(' ', characterLimit);
+    text = text.substring(0, lastSpaceIndex);
+    h3Element.textContent = text.slice(0, characterLimit) + '...';
+  }
+});
