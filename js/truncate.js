dfb2cf19ede33ed08fe642efb4946710cd3ba4b3
Wendy03 <wendysu03@gmail.com>
2023-10-04 15:11:12 +0800

    update: content

--- js/truncate.js
+++ js/truncate.js
@@ -1,11 +1,13 @@
-const h3Elements = document.querySelectorAll('h3.truncateText');
+(function () {
+  const h3Elements = document.querySelectorAll('h3.truncateText');
 
-h3Elements.forEach(function (h3Element) {
-  let text = h3Element.textContent.trim();
-  const characterLimit = 50;
-  if (text.length > characterLimit) {
-    const lastSpaceIndex = text.lastIndexOf(' ', characterLimit);
-    text = text.substring(0, lastSpaceIndex);
-    h3Element.textContent = text.slice(0, characterLimit) + '...';
-  }
-});
+  h3Elements.forEach(function (h3Element) {
+    let text = h3Element.textContent.trim();
+    const characterLimit = 50;
+    if (text.length > characterLimit) {
+      const lastSpaceIndex = text.lastIndexOf(' ', characterLimit);
+      text = text.substring(0, lastSpaceIndex);
+      h3Element.textContent = text.slice(0, characterLimit) + '...';
+    }
+  });
+})();
